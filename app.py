fd0d73ee03612bac13a488208800ef74744b8d5b
JJ2902 <jessicaxyp@gmail.com>
2025-01-13 10:10:06 +0000

    backup

--- app.py
+++ app.py
@@ -1,12 +1,72 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.album_respository import *
+from lib.artist_repository import *
+from lib.album import *
+from lib.artist import *
 
 # Create a new Flask app
 app = Flask(__name__)
 
 # == Your Routes Here ==
+@app.route('/albums', methods=['POST'])
+def post_album():
+    if has_invalid_album_parameters(request.form):
+        return "You need to submit a title, release_year, and artist_id", 400
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    album = Album(
+        None,
+        request.form['title'],
+        request.form['release_year'],
+        request.form['artist_id'])
 
+    repository.create(album)
+    return '', 200
+
+@app.route('/albums', methods=['GET'])
+def get_albums():
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    albums = repository.all()
+    return "\n".join(
+        f"{album}" for album in albums
+    )
+
+def has_invalid_album_parameters(form):
+    return 'title' not in form or \
+        'release_year' not in form \
+        or 'artist_id' not in form
+
+#Artist
+@app.route('/artists', methods=['GET'])
+def get_artists():
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    artists = repository.all()
+    return "\n".join(
+        f"{artist}" for artist in repository.all()
+    )
+
+@app.route('/artists', methods=['POST'])
+def post_artist():
+    if has_invalid_artist_parameters(request.form):
+        return "You need to submit an artist name and genre.", 400
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    artist = Artist(
+        None,
+        request.form['name'],
+        request.form['genre']
+    )
+
+    repository.create(artist)
+    return 'Artist created successfully', 200
+
+def has_invalid_artist_parameters(form):
+    return 'name' not in form or \
+        'genre' not in form
 
 # == Example Code Below ==
 
